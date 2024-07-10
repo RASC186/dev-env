8ba1d988887afe8955f3d838f3aaaa4faaaecb8f
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-07-09 23:58:19 -0300

    Add tmux plugins directory to docker volumes

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 sudo docker run --privileged --net=host \
   -v workspace:/root/workspace \
+  -v tmux_data:/root/.config/tmux/plugins/ \
   -v nvim_data:/root/.local/share/nvim/ \
   -it dev-container /bin/bash
