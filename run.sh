bd189b54c60a265842ddb3c3aad570213693e2ba
Ramon Andrade Sousa Carvalho <ramon.andrade@live.com>
2024-07-09 23:57:31 -0300

    Set docker run command net option as 'host'

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo docker run --privileged \
+sudo docker run --privileged --net=host \
   -v workspace:/root/workspace \
   -v nvim_data:/root/.local/share/nvim/ \
   -it dev-container /bin/bash
